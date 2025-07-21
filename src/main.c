454d5eb7f549ab826e91687e63d511fef1d2358b
Cody Gu <gujiaqi@iscas.ac.cn>
2025-07-21 17:31:01 +0800

    fix: print delay

--- src/main.c
+++ src/main.c
@@ -10,11 +10,11 @@ int main(void)
     const struct device *const encoder2 = DEVICE_DT_GET(DT_ALIAS(encoder2));
     /* 检查设备是否就绪 */
     if (!device_is_ready(encoder1)) {
-        printk("Error: Encoder1 device is not ready\n");
+        printf("Error: Encoder1 device is not ready\n");
         return 0;
     }
     if (!device_is_ready(encoder2)) {
-        printk("Error: Encoder2 device is not ready\n");
+        printf("Error: Encoder2 device is not ready\n");
         return 0;
     }
 
@@ -25,37 +25,37 @@ int main(void)
         int rc;
         rc = sensor_sample_fetch(encoder1);
         if (rc != 0) {
-            printk("Failed to fetch sample from encoder1 (%d)\n", rc);
+            printf("Failed to fetch sample from encoder1 (%d)\n", rc);
             return 0;
         }
 
         rc = sensor_channel_get(encoder1, SENSOR_CHAN_ROTATION, &val1_rotation);
         if (rc != 0) {
-            printk("Failed to get val1_rotation (%d)\n", rc);
+            printf("Failed to get val1_rotation (%d)\n", rc);
             return 0;
         }
 
         rc = sensor_channel_get(encoder1, SENSOR_CHAN_REVOLUTIONS, &val1_revolution);
         if (rc != 0) {
-            printk("Failed to get val1_revolution (%d)\n", rc);
+            printf("Failed to get val1_revolution (%d)\n", rc);
             return 0;
         }
 
         rc = sensor_sample_fetch(encoder2);
         if (rc != 0) {
-            printk("Failed to fetch sample from encoder2 (%d)\n", rc);
+            printf("Failed to fetch sample from encoder2 (%d)\n", rc);
             return 0;
         }
 
         rc = sensor_channel_get(encoder2, SENSOR_CHAN_ROTATION, &val2_rotation);
         if (rc != 0) {
-            printk("Failed to get val2_rotation (%d)\n", rc);
+            printf("Failed to get val2_rotation (%d)\n", rc);
             return 0;
         }
 
         rc = sensor_channel_get(encoder2, SENSOR_CHAN_REVOLUTIONS, &val2_revolution);
         if (rc != 0) {
-            printk("Failed to get val2_revolution (%d)\n", rc);
+            printf("Failed to get val2_revolution (%d)\n", rc);
             return 0;
         }
 
@@ -64,7 +64,7 @@ int main(void)
         angle2 = (val2_rotation.val1 + val2_rotation.val2 / 1000000.0f) + val2_revolution.val1 * 360;
 
         /* 通过串口打印结果 */
-        printk("%.2f, %.2f\n",
+        printf("%.2f, %.2f\n",
                (double)angle1, (double)angle2);
 
         /* 每100ms更新一次 */
