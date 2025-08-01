788be162c7ff09f12e27f2f8112a38cf01bffad1
Cody Gu <gujiaqi@iscas.ac.cn>
2025-08-01 16:58:19 +0800

    fix: xrce-dds can now be compiled

--- src/main.c
+++ src/main.c
@@ -1,65 +1,114 @@
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/sensor.h>
+#include "HelloWorld.h"
+#include "microxrce_transports.h"
+
+#include <uxr/client/client.h>
+#include <ucdr/microcdr.h>
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define UPDATE_INTERVAL_MS 10  // 500ms update interval
+#define STREAM_HISTORY  8
+#define BUFFER_SIZE     UXR_CONFIG_CUSTOM_TRANSPORT_MTU * STREAM_HISTORY
 
-static const struct device *accel = DEVICE_DT_GET(DT_NODELABEL(bmi08x_accel));
-static const struct device *gyro = DEVICE_DT_GET(DT_NODELABEL(bmi08x_gyro));
-
-int main(void)
+void main(void)
 {
-    if (!device_is_ready(accel)) {
-        printf("Accelerometer device not ready\n");
-        return -1;
+    uxrCustomTransport transport;
+    uxr_set_custom_transport_callbacks(&transport,
+                                        true,
+                                        zephyr_transport_open,
+                                        zephyr_transport_close,
+                                        zephyr_transport_write,
+                                        zephyr_transport_read);
+    // Transport
+    if(!uxr_init_custom_transport(&transport, &default_params))
+    {
+        printf("Error at create transport.\n");
+        while(1){};
     }
 
-    if (!device_is_ready(gyro)) {
-        printf("Gyroscope device not ready\n");
-        return -1;
+    // Session
+    uxrSession session;
+    uxr_init_session(&session, &transport.comm, 0xAAAABBBB);
+    if(!uxr_create_session(&session))
+    {
+        printf("Error at create session.\n");
+        while(1){};
     }
 
-    printf("BMI088 sensor initialization completed, starting data output...\n");
-    printf("----------------------------------------\n");
+    // Streams
+    uint8_t output_reliable_stream_buffer[BUFFER_SIZE];
+    uxrStreamId reliable_out = uxr_create_output_reliable_stream(&session, output_reliable_stream_buffer, BUFFER_SIZE, STREAM_HISTORY);
 
-    while (1) {
-        struct sensor_value accel_data[3], gyro_data[3];
-        int ret;
+    uint8_t input_reliable_stream_buffer[BUFFER_SIZE];
+    uxr_create_input_reliable_stream(&session, input_reliable_stream_buffer, BUFFER_SIZE, STREAM_HISTORY);
 
-        /* Read accelerometer data */
-        ret = sensor_sample_fetch(accel);
-        if (ret == 0) {
-            sensor_channel_get(accel, SENSOR_CHAN_ACCEL_X, &accel_data[0]);
-            sensor_channel_get(accel, SENSOR_CHAN_ACCEL_Y, &accel_data[1]);
-            sensor_channel_get(accel, SENSOR_CHAN_ACCEL_Z, &accel_data[2]);
+    // Create entities
+    uxrObjectId participant_id = uxr_object_id(0x01, UXR_PARTICIPANT_ID);
+    const char* participant_xml = "<dds>"
+                                      "<participant>"
+                                          "<rtps>"
+                                              "<name>default_xrce_participant</name>"
+                                          "</rtps>"
+                                      "</participant>"
+                                  "</dds>";
+    uint16_t participant_req = uxr_buffer_create_participant_xml(&session, reliable_out, participant_id, 0, participant_xml, UXR_REPLACE);
 
-            printf("Accelerometer data: X=%.2f m/s², Y=%.2f m/s², Z=%.2f m/s²\n",
-                   sensor_value_to_double(&accel_data[0]),
-                   sensor_value_to_double(&accel_data[1]),
-                   sensor_value_to_double(&accel_data[2]));
-        } else {
-            printf("Failed to read accelerometer data: %d\n", ret);
-        }
+    uxrObjectId topic_id = uxr_object_id(0x01, UXR_TOPIC_ID);
+    const char* topic_xml = "<dds>"
+                                "<topic>"
+                                    "<name>HelloWorldTopic</name>"
+                                    "<dataType>HelloWorld</dataType>"
+                                "</topic>"
+                            "</dds>";
+    uint16_t topic_req = uxr_buffer_create_topic_xml(&session, reliable_out, topic_id, participant_id, topic_xml, UXR_REPLACE);
 
-        /* Read gyroscope data */
-        ret = sensor_sample_fetch(gyro);
-        if (ret == 0) {
-            sensor_channel_get(gyro, SENSOR_CHAN_GYRO_X, &gyro_data[0]);
-            sensor_channel_get(gyro, SENSOR_CHAN_GYRO_Y, &gyro_data[1]);
-            sensor_channel_get(gyro, SENSOR_CHAN_GYRO_Z, &gyro_data[2]);
+    uxrObjectId publisher_id = uxr_object_id(0x01, UXR_PUBLISHER_ID);
+    const char* publisher_xml = "";
+    uint16_t publisher_req = uxr_buffer_create_publisher_xml(&session, reliable_out, publisher_id, participant_id, publisher_xml, UXR_REPLACE);
 
-            printf("Gyroscope data:   X=%.2f rad/s, Y=%.2f rad/s, Z=%.2f rad/s\n",
-                   sensor_value_to_double(&gyro_data[0]),
-                   sensor_value_to_double(&gyro_data[1]),
-                   sensor_value_to_double(&gyro_data[2]));
-        } else {
-            printf("Failed to read gyroscope data: %d\n", ret);
-        }
+    uxrObjectId datawriter_id = uxr_object_id(0x01, UXR_DATAWRITER_ID);
+    const char* datawriter_xml = "<dds>"
+                                     "<data_writer>"
+                                         "<topic>"
+                                             "<kind>NO_KEY</kind>"
+                                             "<name>HelloWorldTopic</name>"
+                                             "<dataType>HelloWorld</dataType>"
+                                         "</topic>"
+                                     "</data_writer>"
+                                 "</dds>";
+    uint16_t datawriter_req = uxr_buffer_create_datawriter_xml(&session, reliable_out, datawriter_id, publisher_id, datawriter_xml, UXR_REPLACE);
 
-        printf("----------------------------------------\n");
-        k_msleep(UPDATE_INTERVAL_MS);
+    // Send create entities message and wait its status
+    uint8_t status[4];
+    uint16_t requests[4] = {participant_req, topic_req, publisher_req, datawriter_req};
+    if(!uxr_run_session_until_all_status(&session, 1000, requests, status, 4))
+    {
+        printf("Error at create entities: participant: %i topic: %i publisher: %i darawriter: %i\n", status[0], status[1], status[2], status[3]);
+        while(1){};
     }
 
-    return 0;
+    // Write topics
+    bool connected = true;
+    uint32_t count = 0;
+    while(connected)
+    {
+        HelloWorld topic = {++count, "Hello DDS world!"};
+
+        ucdrBuffer ub;
+        uint32_t topic_size = HelloWorld_size_of_topic(&topic, 0);
+        uxr_prepare_output_stream(&session, reliable_out, datawriter_id, &ub, topic_size);
+        HelloWorld_serialize_topic(&ub, &topic);
+
+        printf("Send topic: %s, id: %i\n", topic.message, topic.index);
+        connected = uxr_run_session_time(&session, 1000);
+    }
+
+    // Delete resources
+    uxr_delete_session(&session);
+    uxr_close_custom_transport(&transport);
+
+    while(1){};
 }
